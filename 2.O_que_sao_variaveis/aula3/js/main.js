eb8990e2a6211e5c77c0fddaa8fd67c1f105f0c2
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-05 01:54:21 -0300

    exercicio

---
+++ 2.O_que_sao_variaveis/aula3/js/main.js
@@ -0,0 +1,23 @@
+// Pedir para o usuario digitar um numero
+let n1 = prompt("Digite um número");
+                                    
+// Converter em numero inteiro
+n1 = parseInt(n1);
+                                    
+// Pedir para o usuario digitar outro numero
+let n2 = prompt("Digite outro número");
+                        
+// Converter em numero inteiro
+n2 = parseInt(n2);
+                        
+// Mostrar a subtração concatenando com o texto "a subtração é: ..."
+alert(`O resultado da subtração é: ${(n1-n2)}`);
+                        
+// Mostrar a multiplicação concatenando com o texto "a multiplicação é: ..."
+ alert(`O resultado da multiplicação é: ${(n1*n2)}`);
+
+// Mostrar a divisão concatenando com o texto "a divisão é: ..."
+alert(`O resultado da divisão é: ${(n1/n2)}`);
+                        
+// Mostrar a soma concatenando com o texto "a soma é: ..."
+alert(`O resultado da soma é: ${(n1+n2)}`);
