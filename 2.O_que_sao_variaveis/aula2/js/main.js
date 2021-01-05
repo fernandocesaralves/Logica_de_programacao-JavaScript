a60923a32ba59a6d714ab601c352387fdef1837f
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-05 00:14:09 -0300

    Conversao entre tipos

---
+++ 2.O_que_sao_variaveis/aula2/js/main.js
@@ -0,0 +1,11 @@
+// Pedir para o usuário digitar um número 
+let n1 = prompt("Digite um número");
+n1 = parseFloat(n1);
+
+// Pedir para o usuário digitar outro número 
+let n2 = prompt("Digite outro número");
+n2 = parseFloat(n2);
+
+// Mostrar a soma
+let soma = (n1 + n2);
+alert(`O resultado da soma é ${soma}`);
