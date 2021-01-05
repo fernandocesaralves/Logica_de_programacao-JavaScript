7f75c7d60dd9dea507622fec7d4ee194cf5bcdad
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-05 07:15:45 -0300

    Operadores aritmeticos

---
+++ 2.O_que_sao_variaveis/aula4/js/main.js
@@ -0,0 +1,26 @@
+// + Adição 
+
+// - Subtração 
+
+// * Multiplicação 
+
+// / Divisão 
+
+// % Módulo 
+
+// ** Exponenciação 
+
+// ++ Incremento 
+
+// -- Decremento 
+
+// Pedir para o usuário digitar dois números e mostrar a média 
+
+let n1 = prompt("Digite um número");
+n1 = parseFloat(n1);
+
+let n2 = prompt("Digite outro número");
+n2 = parseFloat(n2);
+
+let media = (n1 + n2)/2;
+alert("A média é: " + media);
