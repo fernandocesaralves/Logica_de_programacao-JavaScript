b2f3ca3d4ceb7406434040d4043dd7da739ca994
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-05 22:45:12 -0300

    Operadores de comparacao

---
+++ 2.O_que_sao_variaveis/aula9/js/main.js
@@ -0,0 +1,83 @@
+// Pedir para o usuário digitar dois números e mostrar um boolean indicando se a média for maior que 5
+
+// Operadores de Comparação 
+
+// == Igualdade (valor)
+
+// != Diferente (valor)
+
+// === Igualdade (valor e tipo)
+
+// > Maior 
+
+// >= Maior ou igual 
+
+// < Menor 
+
+// Menor ou igual 
+
+/*
+
+let n1 = prompt("Digite um número:");
+n1 = parseFloat(n1);
+
+let n2 = parseFloat(prompt("Digite outro número"));
+
+let media = (n1 + n2) / 2;
+
+alert( media > 5);
+
+*/
+
+let n1 = parseFloat(prompt("Digite um número"));
+
+let n2 = parseFloat(prompt("Digite outro número"));
+
+let media = (n1 + n2) / 2;
+
+alert( media > 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
