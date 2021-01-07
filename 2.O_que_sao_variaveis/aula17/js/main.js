f19bdc11e4d5fcb44fa796c3b8a8d69893f56b8f
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-07 04:07:59 -0300

    Controle de decisao switch

---
+++ 2.O_que_sao_variaveis/aula17/js/main.js
@@ -0,0 +1,80 @@
+// PEdir para o usuário digitar um número de 1 a 6. Mostrar na tela uma mensagem diferente para cada número 
+
+/* 
+
+let num = parseInt(prompt("Digite um número"));
+
+switch(num) {
+    case 1: 
+        alert("Boa escolha");
+        break;
+    case 2:
+        alert("Legal");
+        break;
+    case 3: 
+        alert("caramba");
+        break;
+    default:
+        alert("Escolha de 1 à 3");
+}
+
+*/
+
+let num = parseInt(prompt("Digite um número"));
+
+switch(num) {
+    case 1: 
+        alert("Olá");
+        break;
+    case 2: 
+        alert("Como você está?");
+        break;
+    case 3:
+        alert("Será que amanhã vai fazer sol?");
+        break;
+    case 4:
+        alert("Você está feliz?");
+        break;
+    default:
+        alert("Escolha um número de 1 à 4");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
