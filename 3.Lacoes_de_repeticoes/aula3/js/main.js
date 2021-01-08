92f363c7f57f789b8464f8e69a979ca032ce7fab
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-08 10:06:53 -0300

    Exercio proposto

---
+++ 3.Lacoes_de_repeticoes/aula3/js/main.js
@@ -0,0 +1,14 @@
+/*
+Para esta tarefa reescreva o código da aula anterior, mas inserindo uma linha divisória sempre que mudar a dezena. 
+Ou seja, entre o 10 e o 11, entre o 20 e o 21 e assim por diante.
+*/
+
+// Dica: para escrever uma linha horizontal, use a tag <hr>
+let num = parseInt(prompt("Digite um número"));
+
+for (let i = 1; i <= 100; i ++) {
+    document.write(`${num} X ${i} = ${(num*i)}<br/>`);
+    if(i % 10 == 0) {
+        document.write("<hr/>")
+    };
+};
