4db32264c3d30bf84d0c2c73b82a5c23b6130ae3
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-08 17:46:11 -0300

    exercicio proposto resolucao

--- 3.Lacoes_de_repeticoes/aula3/js/main.js
+++ 3.Lacoes_de_repeticoes/aula3/js/main.js
@@ -4,11 +4,26 @@ Ou seja, entre o 10 e o 11, entre o 20 e o 21 e assim por diante.
 */
 
 // Dica: para escrever uma linha horizontal, use a tag <hr>
-let num = parseInt(prompt("Digite um número"));
 
+let num = parseInt(prompt("Digite um número"));
+/*
 for (let i = 1; i <= 100; i ++) {
     document.write(`${num} X ${i} = ${(num*i)}<br/>`);
     if(i % 10 == 0) {
         document.write("<hr/>")
     };
 };
+*/
+
+let indice  = 0;
+
+while(indice <= 1000) {
+    document.write(num + ' X ' + indice + ' = ' + (num * indice) + '<br/>');
+
+    if(indice % 10 === 0 && indice > 0) {
+        document.write('<hr/>');
+    }
+
+    indice = indice + 1;
+
+}
