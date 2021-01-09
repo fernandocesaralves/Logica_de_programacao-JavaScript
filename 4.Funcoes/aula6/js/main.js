adb4aaca37fb67f77a02fbd7bad6a20ab53a6da6
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-09 02:58:24 -0300

    recursos do javascript

---
+++ 4.Funcoes/aula6/js/main.js
@@ -0,0 +1,45 @@
+// Pedir para o usuário digitar um número e mostrar a tabuada desse numero de 1 à 1000.
+
+// Deve ter a possibilidade do usuário digitar outro número e também de limpar a tabuada gerada previamente 
+/*
+let output = document.getElementById("output");
+
+let mensagem = '';
+
+for(let i = 0; i < 1001; i++) {
+    mensagem += 'i: ' + i + '<br/>';
+}
+
+output.innerHTML = mensagem;
+*/
+
+/*
+let output = document.querySelector('#output');
+let mensagem = '';
+
+for ( let i = 0; i <= 10; i++ ) {
+    mensagem += 'i: ' + i + '<br/>';
+}
+
+output.innerHTML = mensagem;
+*/
+
+/*
+let output = document.querySelector('#output');
+let mensagem = '';
+
+for ( let i = 0; i <= 10; i++ ) {
+    mensagem += 'i: ' + i + '<br/>';
+}
+
+output.innerHTML = mensagem;
+*/
+
+let output = document.querySelector('#output');
+let mensagem = '';
+
+for( let i = 0; i <= 10; i++ ) {
+    mensagem += 'i: ' + i + '<br/>';
+}
+
+output.innerHTML = mensagem;
