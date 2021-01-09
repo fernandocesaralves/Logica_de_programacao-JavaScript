9855a32ddf273c6e9ad03a3df7da1bafa5cce97c
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-09 12:05:34 -0300

    criando uma funcao

---
+++ 4.Funcoes/aula7/js/main.js
@@ -0,0 +1,18 @@
+// Pedir para o usuário digitar um número e mostrar a tabuada desse numero de 1 a 1000
+
+// Deve ter a possibilidade do usuário digitar outro número e também de limpar a tabuada gerada previamente 
+
+let output = document.querySelector('#output');
+let mensagem = '';
+
+/*
+function mostrarTabuada() {
+    alert("mostarTabuada chamada");
+}
+*/
+
+output.innerHTML = mensagem;
+
+function mostrarTabuada() {
+    console.log("mostrarTabuada foi invocada");
+}
